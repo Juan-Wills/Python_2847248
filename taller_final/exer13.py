f7efd330bdca99fecb8a18d5d2b9f940fb7a7f4a
Juan-Wills <juandavidwills@gmail.com>
2025-06-11 09:26:11 -0500

    CSV files exercise on going

--- taller_final/exer13.py
+++ taller_final/exer13.py
@@ -6,15 +6,76 @@ Implementa funciones para:
 · Manejar errores de archivo no encontrado
 """
 import csv
+import sys
+from datetime import datetime, date
 from tabulate import tabulate
 
 def read(csv_file, new_file):
+
+    exist([csv_file])
+
     with open(csv_file, newline='') as csvfile:
-        reader= csv.DictReader(csvfile)
-        with open(new_file, newline='', mode='w') as nf:
+        reader= csv.reader(csvfile)
+        with open(new_file, 'w', newline='') as nf:
             new_file= csv.writer(nf)
-            formated= [tabulate(reader, headers='firstrow', tablefmt='github')]
+            formated= [tabulate(reader, headers='firstrow', tablefmt='github', numalign='left')]
             new_file.writerow(formated)
 
-read('ventas.csv', 'sales.csv')
+
+def total(file: str, search, new_file):
+
+    exist([file])
+
+    cols= ['Order Date', 'Item Type', 'Units Sold', 'Unit Price']
+
+    # Mapping source file
+    f= open(file, newline='', encoding= 'utf-8')
+    reader= csv.DictReader(f)
+    data=[row for row in reader]
+
+    # Filter data modes
+    if 1 <= search <= 12: 
+        by_date(data, search)
+    elif isinstance(search, str):
+        by_product(data, search)
+    else:
+        raise ValueError(f"argument {search} is invalid.")
+
+    # Create new file
+    with open(new_file, 'w', newline='') as nf:
+        writer= csv.DictWriter(nf, fieldnames= cols, extrasaction= 'ignore')
+        writer.writeheader()
+        for row in reader:
+            writer.writerow(row)
+
+    f.close()
+
+def by_date(file, m):
+    filtered= []
+    for data in file:
+        month= datetime.strptime(data['Order Date'], '%m/%d/%Y').month
+        if month == m:
+            data['Total']= 0
+            filtered.append(data)
+
+    return filtered
+
+
+def by_product(file, p):
+    filtered= []
+    for data in file:
+        pass
+
+
+def exist(files:list):
+    try:
+        for file in files:
+            open(file).close()
+    except FileNotFoundError:
+        sys.exit(f"Error: Archivo no encontrado '{file}'")
+        
+
+total('ventas.csv', 2, 'total.csv')
+
+
 
---
+++ taller_final/utilidades.py
@@ -0,0 +1,59 @@
+"""
+Crea un módulo utilidades.py con funciones matemáticas útiles:
+· Verificar si un número es primo
+· Calcular el máximo común divisor
+· Convertir entre diferentes bases numéricas
+"""
+import re
+
+def isprimo(num: int):
+    if not isinstance(num, int):
+        raise ValueError("Argument must be an integer")
+
+    for div in range(2, num + 1):
+        if num % div == 0 and div != num:
+            return False
+
+    return True
+
+
+def mcd(num1, num2):
+    if not isinstance(num1, int) and  isinstance(num2, int):
+        raise ValueError("Elements must be integers")
+
+    if num2 > num1:
+        num1, num2 = num2, num1
+
+    while num2:
+        num1, num2 = num2, num1 % num2
+
+    return num1
+
+
+def numbases(num, base):
+    if not isinstance(num, int) and isinstance(base, int):
+        raise ValueError("Elements must be integers")
+
+    reminder= ''
+    while num:            
+        reminder+= str(num % base)
+        num//= base
+
+    result= reminder[::-1]
+
+    if base == 16:
+        hex= {'01':'A', '11':'B', '21':'C', '31':'D', '41':'E', '51':'F'}
+        for keys in hex:
+            if keys in reminder[::-1]:
+                result= re.sub(keys, hex[keys], result)
+
+        return result
+
+def main():
+    # print(isprimo(2), mcd(2, 3))
+    return
+
+print(numbases(126, 16))
+
+if __name__ == "__main__":
+    main()
